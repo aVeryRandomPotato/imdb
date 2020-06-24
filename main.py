da7b25181bc6b0d89d1f7c8c7ffab1b6ef2a079d
Chase4823 <chase.stancik@gmail.com>
2020-06-24 00:44:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import sqlite3
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib import style
+#connect to database file
+con = sqlite3.connect("imdb.db")
+#gets rid of the tuples that are returned by default
+con.row_factory = lambda cursor, row: row[0]
+#create cursor object to run queries
+cur = con.cursor()
+directors = cur.execute('''SELECT director_name
+ FROM movies
+ where director_name is not ''
+ group by director_name
+ order by count(director_name) desc
+ limit 10''').fetchall()
+movies = cur.execute('''SELECT count(director_name) as films
+ FROM movies
+ where director_name is not ''
+ group by director_name
+ order by films desc
+ limit 10''').fetchall()
+#close the connection
+con.close()
+fig= plt.figure()
+fig.set_size_inches(9,6)
+total = sum(movies)
+plt.pie(movies,(.1,0,0,0,0,0,0,0,0,0),labels = directors,
+ autopct=lambda p: '{}'.format(int(p * total / 100)),
+ shadow = True, startangle=90)
+plt.title("Directors with the most movies\nAccording to IMDb")
+#plt.show()
+fig.savefig('directors.png')
